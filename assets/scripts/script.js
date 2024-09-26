7b55698cb4917b76be6661d309bd2f6d034b4619
Reggie <reggie.senal@gmail.com>
2024-09-26 11:44:58 +0800

    save theme to localStorage and retrieve back and apply changes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,14 +5,28 @@ const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
+const currentTheme = localStorage.getItem('theme');
+console.log({ currentTheme });
 
 const toggleTheme = (isDarkMode) => {
+    var themeColor = isDarkMode ? 'dark' : 'light';
+    nav.classList.remove('dark-mode');
+    textBox.classList.remove('dark-mode');
     if (isDarkMode) {
-        // change to dark more
+        // change to dark theme
         document.documentElement.setAttribute('data-theme', 'dark');
+        nav.classList.add('dark-mode');
+        textBox.classList.add('dark-mode');
+        toggleIcon.children[0].textContent = "Dark Mode";
+        toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+        changeImage('dark');
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        toggleIcon.children[0].textContent = "Light Mode";
+        toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+        changeImage('light');
     }
+    localStorage.setItem('theme', themeColor);
 }
 
 const changeImage = (imagecolor) => {
@@ -23,22 +37,19 @@ const changeImage = (imagecolor) => {
 
 // switch theme here
 const switchTheme = (event) => {
-    var isDarkMode = event.target.checked;
     console.log('change theme here: ', event.target.checked);
+    var isDarkMode = event.target.checked;
     toggleTheme(isDarkMode);
-    nav.classList.remove('dark-mode');
-    textBox.classList.remove('dark-mode');
-    if (isDarkMode) {
-        nav.classList.add('dark-mode');
-        textBox.classList.add('dark-mode');
-        toggleIcon.children[0].textContent = "Dark Mode";
-        toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-        changeImage('dark');
-    } else {
-        toggleIcon.children[0].textContent = "Light Mode";
-        toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-        changeImage('light');
+}
+
+const loadThemeColor = () => {
+    if (!currentTheme) {
+        currentTheme = 'light';
     }
+    var isDarkMode = currentTheme == 'dark';
+    toggleSwitch.checked = isDarkMode;
+    toggleTheme(isDarkMode);
 }
 // toggle switch change event
 toggleSwitch.addEventListener('change', switchTheme);
+loadThemeColor();
